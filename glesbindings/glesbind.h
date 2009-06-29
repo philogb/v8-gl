8d8a83f70458034d533955371a9844acc0a89be2
Dean McNamee <dean@gmail.com>
2009-06-29 12:28:56 +0200

    Improve building of ES 2.0 on desktop OpenGL.
    - Make sure to GL_GLEXT_PROTOTYPES to get extension functions.
    - Add a shim header for some desktop -> ES 2.0 mappings.

--- glesbindings/glesbind.h
+++ glesbindings/glesbind.h
@@ -11,7 +11,14 @@
 #ifdef __APPLE__
 #include <OpenGL/OpenGL.h>
 #else
+#define GL_GLEXT_PROTOTYPES
 #include <GL/gl.h>
+#endif
+
+// If we're running on desktop OpenGL, some ES 2.0 constants don't exist, or
+// are under a name with EXT in them, etc.
+#ifndef GL_ES_VERSION_2_0
+#include "gles_desktop_shim.h"
 #endif
 
 using namespace std;
